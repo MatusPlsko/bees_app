51cabfb41eb15e93d13721f521056f1c2f014342
Matus Plsko <matusplsko10@gmail.com>
2026-02-24 18:49:27 +0100

    path for bees server

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: "/bees/",
+  
   plugins: [react()],
   server: {
     host: true,
